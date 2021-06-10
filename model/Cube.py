d2bebac3454fabbf9980078a6c4604c67d10ee9f
Daniele Simas Guimaraes <dsg.snf20@uea.edu.br>
2021-06-10 10:29:18 -0400

    Add: model - class Cube

--- model/Cube.py
+++ model/Cube.py
@@ -1,16 +1,10 @@
-from control.constants import *
-
 
 class Cube:
 
-    def __init__(self, POS, color, size):
-        self.pos = POS
+    def __init__(self):
+        self.pos = []
         self.dir_x = 1
         self.dir_y = 0
         self.color = color
         self.size = size
 
-    def move(self, dir_x, dir_y):
-        self.dir_x = dir_x
-        self.dir_y = dir_y
-        self.pos = (self.pos[0] + self.dir_x, self.pos[1] + self.dir_y)
