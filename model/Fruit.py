66c81ab26582d52e56244f6fcc008579d9e2859e
tamaturgo <ddst.snf20@uea.edu.br>
2021-06-11 17:38:22 -0400

    Create Empty class

---
+++ model/Fruit.py
@@ -0,0 +1,11 @@
+class Fruit:
+
+    def __init__(self):
+        self.point_value = 1
+        self.pos = []
+
+    def add_point(point_value):
+        pass
+
+    def spawn_fruit(pos, color):
+        pass
---
+++ model/Snake.py
@@ -0,0 +1,23 @@
+class Snake:
+
+    def __init__(self):
+        self.life = true
+        self.body = []
+        self.head = []
+        self.dirx = 1
+        self.diry = 0
+
+    def eat(fruit):
+        pass
+
+    def move(dirx, diry):
+        pass
+
+    def reset(self):
+        pass
+
+
+class SnakeIa(Snake):
+
+    def IA_Move(self):
+        pass
