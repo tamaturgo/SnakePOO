4e09ef2d056a005435313f217876873552638206
Elikson Bastos <eliksonbt.eks@gmail.com>
2021-06-13 21:34:46 -0400

    Implemented interfaces in Snake

--- model/Snake.py
+++ model/Snake.py
@@ -1,12 +1,14 @@
 import pygame
 from control.constants import *
+from model.DrawInterface import *
+from model.MoveInterface import *
 
 size = SIZE_SNAKE
 
 '''Snake Class'''
 
 
-class Snake:
+class Snake(DrawInterface, MoveInterface):
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen
         self.image = pygame.image.load("../assets/body.png").convert()
@@ -15,38 +17,6 @@ class Snake:
         self.length = 1
         self.x = [250]
         self.y = [250]
-
-    def move_left(self):
-        self.direction = 'left'
-
-    def move_right(self):
-        self.direction = 'right'
-
-    def move_up(self):
-        self.direction = 'up'
-
-    def move_down(self):
-        self.direction = 'down'
-
-    def walk(self):
-        for i in range(self.length-1, 0,  -1):
-            self.x[i] = self.x[i-1]
-            self.y[i] = self.y[i-1]
-
-        if self.direction == 'left':
-            self.x[0] -= size
-        if self.direction == 'right':
-            self.x[0] += size
-        if self.direction == 'up':
-            self.y[0] -= size
-        if self.direction == 'down':
-            self.y[0] += size
-
-        self.draw()
-
-    def draw(self):
-        for i in range(self.length):
-            self.parent_screen.blit(self.image, (self.x[i], self.y[i]))
 
     def increase_length(self):
         self.length += 1
